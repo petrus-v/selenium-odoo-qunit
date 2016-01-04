a13ef98f755a087bada3a8e9c771a790c4020957
Pierre Verkest <pverkest@anybox.fr>
2016-01-05 00:20:05 +0100

    allow OCA version format

--- selenium_odoo_qunit/nose_plugins/odoo_qunit.py
+++ selenium_odoo_qunit/nose_plugins/odoo_qunit.py
@@ -13,7 +13,8 @@ class OdooQunit(Plugin):
         super(OdooQunit, self).options(parser, env=env)
         parser.add_option('--odoo-qunit-odoo-version',
                           default=env.get('ODOO_QUNIT_ODOO_VERSION', '9'),
-                          metavar='VERSION', choices=['7', '8', '9'],
+                          metavar='VERSION',
+                          choices=['7', '7.0', '8', '8.0', '9', '9.0'],
                           help="Odoo main version 7, 8 or 9")
         parser.add_option('--odoo-qunit-base-url',
                           default=env.get('ODOO_QUNIT_BASE_URL',
